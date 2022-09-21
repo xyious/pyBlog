ed816241f232c2633fded7a97bb4c288fe4e404d
Alexandra Knopf <xyious@gmail.com>
2022-09-20 22:40:23 -0700

    add post list and post view

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -9,19 +13,18 @@ class Post(models.Model):
         ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250,
-                            unique_for_date='publish')
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,
-                              choices=STATUS_CHOICES,
-                              default='draft')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month,self.publish.day, self.slug])
 
 class Meta:
     ordering = ('-publish',)
