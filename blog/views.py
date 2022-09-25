8768fdf3e64840bfe32b230600a6190b1741e299
Alexandra Knopf <xyious@gmail.com>
2022-09-25 09:51:17 -0700

    add more email stuff

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
 from .forms import EmailPostForm
 
@@ -12,14 +13,19 @@ class PostListView(ListView):
 
 def post_share(request, post_id):
    post = get_object_or_404(Post, id=post_id, status='published')
+   sent = False
    if request.method == 'POST':
       form = EmailPostForm(request.POST)
       if form.is_valid():
          cd = form.cleaned_data
-         # ... send email .... ??
+         post_url = request.build_absolute_uri(post.get_absolute_url())
+         subject = f"{cd['name']} recommends you read " f"{post.title}"
+         message = f"Read {post.title} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
+         send_mail(subject, message, 'admin@xyious.com', [cd['to']])
+         sent = True
    else:
       form = EmailPostForm()
-   return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+   return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 def post_detail(request, year, month, day, post):
    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
