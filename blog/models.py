8b4f50f5f8a8901b9be71c9387263d09c180b3da
Alexandra Knopf <xyious@gmail.com>
2022-09-27 23:16:28 -0700

    finish comments and tags

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -22,6 +23,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year, self.publish.month,self.publish.day, self.slug])
@@ -44,6 +46,6 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ('created',)
-        
+
     def __str__(self):
         return f'Comment by {self.name} on {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from django.core.mail import send_mail
+from django.db.models import Count
+from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
@@ -10,6 +12,25 @@ class PostListView(ListView):
    context_object_name = 'posts'
    paginate_by = 3
    template_name = 'blog/post/list.html'
+
+def post_list(request, tag_slug=None):
+   object_list = Post.published.all()
+   tag = None
+   if tag_slug:
+      tag = get_object_or_404(Tag, slug=tag_slug)
+      object_list = object_list.filter(tags__in=[tag])
+   paginator = Paginator(object_list, 3) # 3 posts in each page
+   page = request.GET.get('page')
+   try:
+      posts = paginator.page(page)
+   except PageNotAnInteger:
+      # If page is not an integer deliver the first page
+      posts = paginator.page(1)
+   except EmptyPage:
+      # If page is out of range deliver last page of results
+      posts = paginator.page(paginator.num_pages)
+   return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
+
 
 def post_share(request, post_id):
    post = get_object_or_404(Post, id=post_id, status='published')
@@ -44,5 +65,9 @@ def post_detail(request, year, month, day, post):
          new_comment.save()
    else:
       comment_form = CommentForm()
+   # List of similar posts
+   post_tags_ids = Post.tags.values_list('id', flat=True)
+   similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+   similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 
-      'new_comment': new_comment, 'comment_form': comment_form})
+      'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts': similar_posts})
